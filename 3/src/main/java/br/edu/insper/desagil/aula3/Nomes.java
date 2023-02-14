f8124bee12678a0b46ed58c978240c01613bd0e9
koeitsu <brun0zalc@hotmail.com>
2023-02-14 15:33:05 -0300

    terminando desafios da aula 3

--- 3/src/main/java/br/edu/insper/desagil/aula3/Nomes.java
+++ 3/src/main/java/br/edu/insper/desagil/aula3/Nomes.java
@@ -1,12 +1,23 @@
 package br.edu.insper.desagil.aula3;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Nomes {
 
 	public Map<String, List<String>> inverte(Map<String, List<String>> sobrenomes) {
-		return null;
+		Map<String, List<String>> invertida = new HashMap<>();
+		for (String nome : sobrenomes.keySet()) {
+			for (String sobrenome : sobrenomes.get(nome)) {
+				if (invertida.containsKey(sobrenome)) {
+					invertida.get(sobrenome).add(nome);
+				} else {
+					List<String> nomes = new ArrayList<>();
+					nomes.add(nome);
+					invertida.put(sobrenome, nomes);
+				}
+			}
+		}
+		return invertida;
 	}
 
 }
