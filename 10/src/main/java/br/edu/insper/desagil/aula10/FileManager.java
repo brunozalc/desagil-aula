391656bc4120f61f97575a21d8ad7e593aa560f1
koeitsu <brun0zalc@hotmail.com>
2023-03-15 17:54:23 -0300

    aula 10

--- 10/src/main/java/br/edu/insper/desagil/aula10/FileManager.java
+++ 10/src/main/java/br/edu/insper/desagil/aula10/FileManager.java
@@ -2,7 +2,9 @@ package br.edu.insper.desagil.aula10;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -16,26 +18,47 @@ public class FileManager {
 		this.decoder = Charset.forName("UTF-8").newDecoder();
 	}
 
-	public String load(String path) {
-		System.out.println("Abrindo leitor");
-		InputStream fileStream = new FileInputStream(path);
-		System.out.println("Leitor aberto");
-
-		Reader fileReader = new InputStreamReader(fileStream, decoder);
-		BufferedReader reader = new BufferedReader(fileReader);
-
-		System.out.println("Lendo conteúdo");
+	private String readContent(BufferedReader reader) throws IOException {
 		String content = "";
 		String line = reader.readLine();
 		while (line != null) {
 			content += line;
 			line = reader.readLine();
 		}
-		content += "\n";
-		System.out.println("Conteúdo lido");
+		return content + "\n";
+	}
+
+	public String load(String path) {
+		System.out.println("Abrindo leitor");
+		InputStream fileStream;
+		try {
+			fileStream = new FileInputStream(path);
+			System.out.println("Leitor aberto");
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("Arquivo não encontrado");
+		}
+		System.out.println("Leitor aberto");
+
+		Reader fileReader = new InputStreamReader(fileStream, decoder);
+		BufferedReader reader = new BufferedReader(fileReader);
+
+		System.out.println("Lendo conteúdo");
+		String content;
+		try {
+			content = readContent(reader);
+			System.out.println("Conteúdo lido");
+		} catch (IOException e) {
+			System.err.println("Erro ao ler conteúdo: " + e.getMessage());
+			content = null;
+		}
 
 		System.out.println("Fechando leitor");
-		reader.close();
+		try {
+			reader.close();
+			System.out.println("Leitor fechado");
+		} catch (IOException e) {
+			System.err.println("Erro ao fechar leitor");
+		}
 		System.out.println("Leitor fechado");
 
 		return content;
@@ -43,15 +66,28 @@ public class FileManager {
 
 	public void save(String path, String content) {
 		System.out.println("Abrindo escritor");
-		FileWriter writer = new FileWriter(path);
-		System.out.println("Escritor aberto");
+		FileWriter writer;
+		try {
+			writer = new FileWriter(path);
+			System.out.println("Escritor aberto");
+		} catch (IOException e) {
+			throw new IllegalArgumentException("Erro ao abrir escritor: " + e.getMessage());
+		}
 
 		System.out.println("Escrevendo conteúdo");
-		writer.write(content);
-		System.out.println("Conteúdo escrito");
+		try {
+            writer.write(content);
+			System.out.println("Conteúdo escrito");
+        } catch (IOException e) {
+			System.err.println("Erro ao escrever conteúdo: " + e.getMessage());
+        }
 
 		System.out.println("Fechando escritor");
-		writer.close();
-		System.out.println("Escritor fechado");
+		try {
+			writer.close();
+			System.out.println("Escritor fechado");
+		} catch (IOException e) {
+			System.err.println("Erro ao fechar escritor");
+		}
 	}
 }
